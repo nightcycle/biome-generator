0ed9260b40c8a56be64d4ee3731aeb3fe9de4003
Nightcycle <77173389+nightcycle@users.noreply.github.com>
2022-09-13 12:41:26 -0600

    Built and published by Moonwave

---
+++ assets/js/8e348416.35c1d38c.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkdocs=self.webpackChunkdocs||[]).push([[654],{63891:s=>{s.exports=JSON.parse('{"sourceUrl":"https://github.com/nightcycle/landmaster/blob/master","baseUrl":"/landmaster/","classOrder":[],"apiCategories":[]}')}}]);
---
+++ assets/js/f4c9040f.80f9261c.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkdocs=self.webpackChunkdocs||[]).push([[945],{7636:e=>{e.exports=JSON.parse('[{"type":"link","href":"/api/Landmaster","label":"\u200bLandmaster"}]')}}]);
